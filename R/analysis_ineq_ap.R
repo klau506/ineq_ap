d2888f550ce2f85cbab4ebda5ed98274b4333291
klau506 <claudia.rodes@bc3research.org>
2025-01-31 15:01:56 +0100

    start analysis code

---
+++ R/analysis_ineq_ap.R
@@ -0,0 +1,380 @@
+require(eurostat)
+require(sf)
+library(dplyr)
+library(ggplot2)
+library(tmap)
+
+
+# ==============================================================================
+#                                  LOAD DATA                                   #
+# ==============================================================================
+## load dummy PM2.5 concentration & premature deaths by NUTS3
+ap <- get(load('data/rfasst_output/tmp_m2_get_conc_pm25.ctry_nuts.output.RData'))
+deaths <- get(load('data/rfasst_output/m3_get_mort_pm25.output.RData'))
+
+## read socioeconomic data
+gdp <- eurostat::get_eurostat('nama_10r_3gdp') %>% # gdp NUTS3
+  filter(unit == 'PPS_EU27_2020_HAB') %>% # Purchasing power standard (PPS, EU27 from 2020), per inhabitant
+  filter(grepl('2021', TIME_PERIOD)) %>% # Much more data in 2021
+  mutate(unit_GDP = 'PPS_EU27_2020_HAB') %>% 
+  select(geo, unit_GDP, GDPpc = values)
+
+pop <- eurostat::get_eurostat('nama_10r_3popgdp') %>% # pop NUTS3
+  filter(grepl('2021', TIME_PERIOD)) %>% # Much more data in 2021
+  mutate(unit_POP = 'THS') %>% 
+  select(geo, unit_POP, POP = values)
+
+pop_ctzha <- eurostat::get_eurostat('cens_21ctzha_r3') %>% # pop_detailed NUTS3
+  filter(housing == 'TOTAL', citizen == 'TOTAL') %>% 
+  select(geo, age_group = age, age = values)
+
+hdd_cdd <- eurostat::get_eurostat('nrg_chddr2_a') %>% # HDD & CDD NUTS3
+  filter(grepl('2023', TIME_PERIOD)) %>% 
+  tidyr::pivot_wider(names_from = 'indic_nrg', values_from = 'values') %>% 
+  mutate(unit_CDD = 'NR',
+         unit_HDD = 'NR') %>% 
+  select(geo, CDD, HDD, unit_CDD, unit_HDD)
+
+grid_dt <- eurostat::get_eurostat_geospatial(year = 2021) %>% # 2024 available, but not for URBN_TYPE, which will not change much from 2021 to 2024
+  as.data.frame() %>% 
+  select(LEVL_CODE, NUTS_ID, CNTR_CODE, URBN_TYPE, geo)
+
+
+socioecon_dt <- purrr::reduce(
+  list(grid_dt, gdp, pop, pop_ctzha, hdd_cdd),
+  full_join,
+  by = 'geo'
+) %>% 
+  select(
+    -starts_with("unit"),
+    starts_with("unit")
+  ) %>% 
+  filter(LEVL_CODE == 3)
+
+
+pop_employed <- eurostat::get_eurostat('nama_10r_3empers') # pop_employed NUTS3
+gva_act <- eurostat::get_eurostat('nama_10r_3gva') # gross value added by act NUTS3
+# pop_fhcs <- eurostat::get_eurostat('cens_21fhcs_r3') # pop_detailed NUTS3
+income <- eurostat::get_eurostat('nama_10r_2hhinc') # income NUTS2 na_item == 'B6N'
+# poverty <- eurostat::get_eurostat('ilc_peps11n') # poverty NUTS2
+
+
+temp_dir <- tempdir()
+unzip("data/Eurostat_Census-GRID_2021_V2.1.zip", exdir = temp_dir)
+sf_data <- sf::st_read(file.path(temp_dir, "ESTAT_Census_2021_V2.gpkg"))
+sf_data <- sf_data %>%
+  select(-ends_with("_CI"))
+
+sf_data_YGE65 <- sf_data %>%
+  dplyr::filter(POPULATED == 1, T >= 0)
+
+pl <- ggplot() +
+  geom_sf(data = sf_data_YGE65, 
+          aes(color = Y_GE65,
+              fill = Y_GE65)) +
+  labs(fill = "Y_GE65") + 
+  ggpubr::theme_pubr() +
+  theme(panel.background = element_rect(fill = 'white'),
+        panel.grid.major = element_line(colour = "grey90"),
+        panel.ontop = FALSE,
+        strip.text = element_text(size = 12),
+        strip.background = element_blank(),
+        axis.title.x = element_text(size = 12),
+        axis.text = element_text(size = 12),
+        legend.key.size = unit(1.5, 'cm'),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.position = "bottom")
+
+ggsave(file='figures/pl_prova.pdf', height = 15, width = 15, units = 'cm',
+       plot = pl)
+
+
+grid_sf <- st_transform(sf_data_YGE65, crs = st_crs(nuts3_plot_data))
+grid_sf_with_nuts3 <- st_join(grid_sf, nuts3_plot_data, join = st_intersects)
+st_write(grid_sf_with_nuts3, "data/tmp_output_grid_with_nuts3.shp", append = FALSE)
+
+grid_sf_with_nuts3_v2 <- as.data.frame(grid_sf_with_nuts3) %>%
+  dplyr::filter(T > 0, Y_GE65 >= 0) %>% 
+  dplyr::select(GRD_ID, Y_GE65, Y_TOT = T, geo)
+
+grid_sf_with_nuts3_v3 <- unique(grid_sf_with_nuts3_v2) %>% 
+  dplyr::group_by(geo) %>%
+  dplyr::summarise(Y_GE65 = sum(Y_GE65),
+                   Y_TOT = sum(Y_TOT)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(per_elderly = ifelse(!is.na(Y_GE65), Y_GE65 / Y_TOT, NA_real_)) %>% 
+  dplyr::group_by(geo) %>%
+  dplyr::summarise(mean_per_elderly = mean(per_elderly, na.rm = TRUE)) %>%
+  dplyr::ungroup()
+
+grid_sf_with_nuts3_v4 <- as.data.frame(grid_sf_with_nuts3_v3) %>% 
+  dplyr::filter(rowSums(is.na(.)) == 0) %>% 
+  dplyr::left_join(nuts3_plot_data %>% 
+                     select(geo, geometry),
+                   by = 'geo')
+
+grid_sf_with_nuts3_v4 <- sf::st_sf(grid_sf_with_nuts3_v4, geometry = grid_sf_with_nuts3_v4$geometry)
+
+plot_elderly <- tm_shape(nuts3_plot_data,
+                         projection = "EPSG:3035",
+                         xlim = c(2400000, 6500000),
+                         ylim = c(1320000, 5650000)
+) +
+  tm_fill("lightgrey") +
+  tm_shape(grid_sf_with_nuts3_v4) +
+  tm_polygons("mean_per_elderly",
+              title = "Elderly people [%]",
+              palette = "Oranges",
+              style = "cont"
+  ) +
+  tm_layout(legend.title.size = 0.8) +
+  tm_layout(legend.text.size = 0.6)
+
+
+tmap::tmap_save(plot_elderly, filename = "figures/plot_elderly21.pdf",
+                width = 100, height = 100, units = 'mm', dpi = 300)
+
+
+
+# ctry_nuts <- as(ctry_nuts_sf, "SpatVector")
+
+# Average raster values by polygon
+ctry_nuts$pm25_avg <- terra::extract(pm25_weighted_nuts3, ctry_nuts, mean, na.rm = TRUE)$layer
+
+
+# 
+# plot_M <- tm_shape(sf_data %>% 
+#                      dplyr::filter(POPULATED == 1, M >= 0)) + 
+#   tm_borders() + 
+#   tm_fill("M", 
+#           title = "M Value", 
+#           palette = "YlOrRd",
+#           style = "cont"
+#   )
+# tmap::tmap_save(plot_M, filename = "figures/plot_prova.pdf",
+#                 width = 100, height = 100, units = 'mm', dpi = 300)
+
+# spacial data for plots
+nuts3_plot_data <- get_eurostat_geospatial(resolution = 3, nuts_level = 3, year = 2021) %>% 
+  dplyr::select(-FID)
+
+# ==============================================================================
+#                              COMPUTE INDICATORS                              #
+# ==============================================================================
+
+#####
+## GINI (index)
+
+#####
+## INCOME PER CAPITA (PPPpc)
+socioecon_dt2 <- socioecon_dt %>% 
+  mutate(total_gdp = GDPpc * POP * 1e3) %>% 
+  group_by(CNTR_CODE) %>% 
+  summarise(total_gdp = sum(total_gdp))
+
+#####
+## ELDERLY PEOPLE (% people >= Y75)
+elderly_age_list <- c("Y75-79","Y80-84","Y85-89","Y90-94","Y95-99","Y_GE100")
+
+socioecon_dt_ind <- socioecon_dt %>% 
+  mutate(total = ifelse(age_group == "TOTAL", age, NA)) %>% # use this "total" pop since it's more precise
+  tidyr::fill(total, .direction = "down") %>% 
+  group_by(geo) %>%
+  mutate(NR_elderly = ifelse(age_group %in% elderly_age_list, 
+                             sum(age[age_group %in% elderly_age_list]), NA)) %>% 
+  tidyr::fill(NR_elderly, .direction = "downup") %>%  
+  mutate(per_elderly = NR_elderly / total * 100) %>% 
+  ungroup() %>% 
+  select(-total, -NR_elderly) %>% 
+  left_join(nuts3_plot_data %>% 
+              select(geo, geometry),
+            by = 'geo')
+socioecon_dt_ind_sf <- sf::st_sf(socioecon_dt_ind, geometry = socioecon_dt_ind$geometry)
+if(nrow(socioecon_dt_ind[rowSums(is.na(socioecon_dt_ind))>0,]) != 0) {
+  warning("There are not-matching NUTS3 codes between the socioecon_dt_ind and EUROSTAT NUTS3 data")
+}
+
+plot_elderly <- tm_shape(nuts3_plot_data,
+                    projection = "EPSG:3035",
+                    xlim = c(2400000, 6500000),
+                    ylim = c(1320000, 5650000)
+) +
+  tm_fill("lightgrey") +
+  tm_shape(socioecon_dt_ind_sf) +
+  tm_polygons("per_elderly",
+              title = "Elderly people [%]",
+              palette = "Oranges",
+              style = "cont"
+  ) +
+  tm_layout(legend.title.size = 0.8) +
+  tm_layout(legend.text.size = 0.6)
+
+
+tmap::tmap_save(plot_elderly, filename = "figures/plot_elderly.pdf",
+                width = 100, height = 100, units = 'mm', dpi = 300)
+
+
+#####
+## URBN_TYPE (1 = cities, 2 = towns and suburbs, 3 = rural)
+socioecon_dt_ind_urbntype_sf <- socioecon_dt_ind_sf %>% 
+  select(geometry, URBN_TYPE) %>% 
+  mutate(URBN_TYPE = factor(URBN_TYPE, 
+                            levels = c(1, 2, 3), 
+                            labels = c("City", "Town/Suburb", "Rural"))) %>%
+  distinct()
+
+plot_urbtype <- tm_shape(nuts3_plot_data,
+                         projection = "EPSG:3035",
+                         xlim = c(2400000, 6500000),
+                         ylim = c(1320000, 5650000)
+) +
+  tm_fill("lightgrey") +
+  tm_shape(socioecon_dt_ind_urbntype_sf) +
+  tm_polygons("URBN_TYPE",
+              title = "Urban type",
+              palette = c("#C288B0", "#C3D2D5", "#92DEC3")
+  ) +
+  tm_layout(legend.title.size = 0.8) +
+  tm_layout(legend.text.size = 0.6)
+
+tmap::tmap_save(plot_urbtype, filename = "figures/plot_urbtype.pdf",
+                width = 100, height = 100, units = 'mm', dpi = 300)
+
+
+
+
+# ==============================================================================
+#                                    ANALYSE                                   #
+# ==============================================================================
+
+## AP
+dt <- ap %>% 
+  filter(nchar(region) > 3,
+         year == 2020) %>% 
+  rename(geo = region,
+         ap = value) %>% 
+  left_join(nuts3_plot_data,
+            by = 'geo')
+dt_sf <- sf::st_sf(dt, geometry = dt$geometry)
+if(nrow(dt[rowSums(is.na(dt))>0,]) != 0) {
+  warning("There are not-matching NUTS3 codes between the rfasst AP results and EUROSTAT NUTS3 data")
+}
+
+plot_ap <- tm_shape(nuts3_plot_data,
+                    projection = "EPSG:3035",
+                    xlim = c(2400000, 6500000),
+                    ylim = c(1320000, 5650000)
+  ) +
+  tm_fill("lightgrey") +
+  tm_shape(dt_sf %>% 
+             dplyr::filter(year == 2020)) +
+  tm_polygons("ap",
+              title = "PM2.5\n[ug/m3]",
+              palette = "Oranges",
+              style = "cont"
+  ) +
+  tm_layout(legend.title.size = 0.8) +
+  tm_layout(legend.text.size = 0.6)
+
+tmap::tmap_save(plot_ap, filename = "figures/plot_ap.pdf",
+                width = 100, height = 100, units = 'mm', dpi = 300)
+
+
+
+
+## AP vs URBN_TYPE
+# graph OX URBN_TYPE i OY nivell de PM. Fer punts, i violin plot?
+# fer test estadístic per a veure si hi ha alguna relació
+
+# check normality
+ap_urbntype_sf <- sf::st_join(
+  socioecon_dt_ind_urbntype_sf,
+  dt_sf %>% 
+    dplyr::select(-URBN_TYPE)
+) %>% 
+  select(geo, URBN_TYPE, ap, geometry)
+
+# Histogram
+ggplot(ap_urbntype_sf, aes(x = ap)) +
+  geom_histogram(bins = 30, fill = "skyblue", color = "black") +
+  facet_wrap(~ URBN_TYPE)
+
+# Q-Q plot
+ggplot(ap_urbntype_sf, aes(sample = ap)) +
+  stat_qq() +
+  stat_qq_line() +
+  facet_wrap(~ URBN_TYPE)
+
+# Anderson-Darling Test
+test <- nortest::ad.test(ap_urbntype_sf$ap)
+# A = 59.643, p-value < 2.2e-16 --> NOT normal distribution
+
+# Kruskal-Wallis Test (for >2 categories) - non parametric test
+test <- kruskal.test(ap ~ URBN_TYPE, data = ap_urbntype_sf)
+# Kruskal-Wallis chi-squared = 401.25, df = 2, p-value < 2.2e-16
+rstatix::kruskal_effsize(data = as.data.frame(ap_urbntype_sf), ap ~ URBN_TYPE)
+# .y.       n effsize method  magnitude
+# * <chr> <int>   <dbl> <chr>   <ord>    
+#   1 ap     8724  0.0458 eta2[H] small    
+# Result: the effect of the URBN_TYPE is SIGNIFICANT but small
+
+
+df <- data.table::as.data.table(ap_urbntype_sf) %>% 
+  select(-geometry)
+df_medi <- df[, .(medi = quantile(ap, 0.5, na.rm = T)),
+              by=c('URBN_TYPE')]
+
+df = data.table::data.table(df)
+
+plot_urbntype_density <- ggplot(df) +
+  geom_density(data = df, aes(x = ap, group = URBN_TYPE,
+                              color = URBN_TYPE, fill = URBN_TYPE), 
+               linewidth = 0.8, alpha = 0.25) +
+  geom_vline(aes(color = URBN_TYPE, xintercept = medi),
+             data = df_medi, linewidth = 1) +
+  facet_wrap(. ~ URBN_TYPE, nrow = 3) +
+  ggpubr::theme_pubr() +
+  scale_fill_manual(values = urbn_type.color,
+                    name = 'Urban type',
+                    labels = urbn_type.labs)+
+  scale_color_manual(values = urbn_type.color,
+                     name = 'Urban type',
+                     labels = urbn_type.labs)+
+  labs(x = 'PM2.5 concentration [ug/m3]', y = 'Probability density') +
+  theme(panel.background = element_rect(fill = 'white'),
+        panel.grid.major = element_line(colour = "grey90"),
+        panel.ontop = FALSE,
+        strip.text = element_text(size = 12),
+        strip.background = element_blank(),
+        axis.title.x = element_text(size = 12),
+        axis.text = element_text(size = 12),
+        legend.key.size = unit(1.5, 'cm'),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.position = "bottom")
+
+ggsave(file='figures/plot_urbntype_density.pdf', height = 15, width = 15, units = 'cm',
+       plot = plot_urbntype_density)
+
+
+
+
+## AP vs ELDERLY
+dt <- socioecon_dt_ind %>% 
+  left_join(ap %>% 
+              filter(level == 'NUTS') %>% 
+              rename(geo = region,
+                     ap = value),
+            by = 'geo',
+            relationship = "many-to-many") %>% 
+  filter(rowSums(is.na(.)) == 0) %>% # TODO fix
+  left_join(nuts3_plot_data)
+dt_sf <- sf::st_sf(dt, geometry = dt$geometry)
+
+plot_nutsID <- ggplot(data = dt_sf) +
+  geom_sf(aes(fill = NUTS_ID)) +
+  labs(title = "NUTS3 Regions", fill = "Value") +
+  theme_minimal() +
+  theme(legend.position = 'none')
+ggsave(plot_ap, file = 'figures/plot_nutsID.pdf', height = 10, width = 10, units = 'cm')
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+urbn_type.color = c('City'='#C288B0',
+                    'Town/Suburb'='#C3D2D5',
+                    'Rural'='#92DEC3')
+urbn_type.labs = c('City','Town/Suburb','Rural')
+names(urbn_type.labs) <- c('City','Town/Suburb','Rural')

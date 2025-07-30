88f53f99bd183fdd947afaacb05fc63ea4863002
klau506 <claudia.rodes@bc3research.org>
2025-07-30 16:10:00 +0200

    data: income from gridded source and agg to nuts3
    instead of agg from AU_level

--- R/data_preprocess.R
+++ R/data_preprocess.R
@@ -127,7 +127,59 @@ urbn_type <- xlsx::read.xlsx('data/NUTS2021.xlsx', sheetName = 'Urban-rural') %>
 save(urbn_type, file = "data/tmp/urbn_type.RData")
 
 
-#### harmonized income data ====================================================
+#### income pc by nuts3 data ===================================================
+pm.ap_raster <- terra::rast("data/rfasst_output/EU_NECP_LTT_2030_pm25_fin_weighted.tif")
+extent_raster <- terra::ext(-26.276, 40.215, 32.633, 71.141)
+pm.ap_raster_crop <- terra::crop(pm.ap_raster, extent_raster)
+
+inc_data <- terra::rast("data/High-resolution_Downscaling/Europe_disp_inc_2015.tif")
+inc_data2 <- terra::resample(inc_data, pm.ap_raster_crop)
+inc_data2 <- terra::crop(inc_data2, extent_raster)
+inc_data2 <- terra::mask(inc_data2, inc_data2, maskvalue = NA)
+
+pop_t <- terra::rast("data/Eurostat_Census-GRID_2021_V2-0/ESTAT_OBS-VALUE-T_2021_V2.tiff")
+pop_t2 <- terra::project(pop_t, pm.ap_raster_crop)
+pop_t2 <- terra::resample(pop_t2, pm.ap_raster_crop)
+pop_t2 <- terra::crop(pop_t2, extent_raster)
+pop_t2 <- terra::mask(pop_t2, pop_t2, maskvalue = NA)
+
+
+# convert the filtered rasters to data frames
+inc_values <- terra::values(inc_data2)
+pop_values <- terra::values(pop_t2)
+
+df_inc <- data.frame('pop' = pop_values, 
+                     'inc_pc' = inc_values)
+colnames(df_inc) <- c('pop','inc_pc')
+df_inc <- df_inc %>% 
+  # total income by grid: inc_grid = inc_pc (by grid) * pop (by grid)
+  dplyr::mutate(inc_grid = inc_pc * pop)
+
+# convert to raster again
+inc_data3 <- inc_data2
+terra::values(inc_data3) = df_inc$inc_grid
+
+pop_data3 <- pop_t2
+terra::values(pop_data3) = df_inc$pop
+
+# make sure the CRS matches
+nuts3_plot_data <- sf::st_transform(nuts3_plot_data, terra::crs(inc_data3))
+nuts3_vect <- terra::vect(nuts3_plot_data)
+
+# extract the sum by nuts3 raster polygon of pop and income
+inc_sum <- terra::extract(inc_data3, nuts3_vect, fun = sum, na.rm = TRUE); colnames(inc_sum)[2] <- 'inc_nuts3'
+pop_sum <- terra::extract(pop_data3, nuts3_vect, fun = sum, na.rm = TRUE); colnames(pop_sum)[2] <- 'pop_nuts3'
+
+# combine with polygon data
+nuts3_with_inc <- dplyr::bind_cols(nuts3_plot_data, inc_sum[,2,drop = F], pop_sum[,2,drop = F])
+
+# compute inc_pc by nuts3
+nuts3_with_inc <- nuts3_with_inc %>% 
+  dplyr::mutate(inc_pc_nuts3 = inc_nuts3 / pop_nuts3)
+
+sf::st_write(nuts3_with_inc, "data/tmp/nuts3_with_inc.shp", append = FALSE)
+
+#### gini by nuts3 data ========================================================
 # Source: https://entrepot.recherche.data.gouv.fr/dataset.xhtml?persistentId=doi:10.57745/TTIOKI
 harm_data <- sf::st_read("data/Replication data/merged_output_2019_wID_clean.shp") %>%
   dplyr::mutate(
@@ -210,20 +262,32 @@ tmap::tmap_save(plot_gdp,
                 width = 100, height = 100, units = "mm", dpi = 300
 )
 
-#### DISPOSABLE INCOME & GINI ==================================================
-harm_data_income_nuts3 <- data.table::as.data.table(harm_data) %>%
-  dplyr::select(Year, ISO, geo = NUTS_ID, Population, Disp_Inc_P) %>%
-  dplyr::filter(rowSums(is.na(.)) == 0) %>%
-  # pop-weighted disposable income:
-  # Disp_Inc_P_nuts3 = SUM_regInNUTS3(Pop * Disp_Inc_P) / SUM_regInNUTS3(Pop)
-  dplyr::mutate(Disp_Inc_P_pw = Disp_Inc_P * Population) %>%
-  dplyr::group_by(Year, geo, ISO) %>%
-  dplyr::mutate(Population_nuts3 = sum(Population)) %>%
-  dplyr::mutate(Disp_Inc_P_nuts3 = sum(Disp_Inc_P_pw) / Population_nuts3) %>%
-  dplyr::ungroup() %>%
-  dplyr::select(Year, ISO, geo, Population_nuts3, Disp_Inc_P_nuts3) %>%
-  dplyr::distinct()
+#### DISPOSABLE INCOME ==================================================
+nuts3_with_inc_sf <- sf::st_sf(nuts3_with_inc, geometry = nuts3_with_inc$geometry)
 
+plot_income <- tm_shape(nuts3_plot_data,
+  projection = "EPSG:3035",
+  xlim = c(2400000, 6500000),
+  ylim = c(1320000, 5650000)
+) +
+  tm_fill("lightgrey") +
+  tm_shape(nuts3_with_inc_sf %>%
+    dplyr::select(inc_pc_nuts3, geometry) %>%
+    dplyr::filter(rowSums(is.na(.)) == 0)) +
+  tm_polygons("inc_pc_nuts3",
+    title = "Disposable Income\n[2015 PPP EU27 €]",
+    palette = "Oranges",
+    style = "cont"
+  ) +
+  tm_layout(legend.title.size = 0.8) +
+  tm_layout(legend.text.size = 0.6)
+
+tmap::tmap_save(plot_income,
+  filename = "figures/plot_income2.pdf",
+  width = 100, height = 100, units = "mm", dpi = 300
+)
+
+#### GINI ==================================================
 harm_data_gini_nuts3 <- data.table::as.data.table(harm_data) %>%
   dplyr::select(Year, ISO, geo = NUTS_ID, Population, Disp_Inc_P, Gini) %>%
   dplyr::filter(rowSums(is.na(.)) == 0) %>%
@@ -235,42 +299,13 @@ harm_data_gini_nuts3 <- data.table::as.data.table(harm_data) %>%
   dplyr::select(Year, ISO, geo, Gini_nuts3) %>%
   dplyr::distinct()
 
-harm_data_nuts3 <- dplyr::full_join(
-  harm_data_income_nuts3,
-  harm_data_gini_nuts3
-)
-
-harm_data_nuts3_sf <- harm_data_nuts3 %>%
+harm_data_gini_nuts3_sf <- harm_data_gini_nuts3 %>%
   dplyr::left_join(
     nuts3_plot_data %>%
       dplyr::select(geo, geometry),
     by = "geo"
   )
-harm_data_nuts3_sf <- sf::st_sf(harm_data_nuts3_sf, geometry = harm_data_nuts3_sf$geometry)
-
-harm_data_nuts3_sf <- get(load('harm_data_nuts3_sf.RData'))
-
-plot_income <- tm_shape(nuts3_plot_data,
-  projection = "EPSG:3035",
-  xlim = c(2400000, 6500000),
-  ylim = c(1320000, 5650000)
-) +
-  tm_fill("lightgrey") +
-  tm_shape(harm_data_nuts3_sf %>%
-    dplyr::select(Disp_Inc_P_nuts3, geometry) %>%
-    dplyr::filter(rowSums(is.na(.)) == 0)) +
-  tm_polygons("Disp_Inc_P_nuts3",
-    title = "Disposable Income\n[2015 PPP EU27 €]",
-    palette = "Oranges",
-    style = "cont"
-  ) +
-  tm_layout(legend.title.size = 0.8) +
-  tm_layout(legend.text.size = 0.6)
-
-tmap::tmap_save(plot_income,
-  filename = "figures/plot_income.pdf",
-  width = 100, height = 100, units = "mm", dpi = 300
-)
+harm_data_gini_nuts3_sf <- sf::st_sf(harm_data_gini_nuts3_sf, geometry = harm_data_gini_nuts3_sf$geometry)
 
 plot_gini <- tm_shape(nuts3_plot_data,
   projection = "EPSG:3035",
@@ -422,10 +457,10 @@ tmap::tmap_save(plot_cdd,
 
 
 harm_socioeconomic_nuts_sf <- data.table::as.data.table(urbn_type) %>%
-  dplyr::full_join(data.table::as.data.table(grid_sf_cdd_v2) %>%
-    dplyr::select(-geometry), by = "geo") %>%
-  dplyr::full_join(data.table::as.data.table(gdp_sf) %>%
-    dplyr::select(geo, gdp), by = "geo") %>%
+  # dplyr::full_join(data.table::as.data.table(grid_sf_cdd_v2) %>%
+  #   dplyr::select(-geometry), by = "geo") %>%
+  # dplyr::full_join(data.table::as.data.table(gdp_sf) %>%
+  #   dplyr::select(geo, gdp), by = "geo") %>%
   dplyr::full_join(data.table::as.data.table(grid_sf_elderly_v3) %>%
                      dplyr::filter(year == 2020) %>% 
                      dplyr::select(geo, sex, per_elderly), 
@@ -435,40 +470,43 @@ harm_socioeconomic_nuts_sf <- data.table::as.data.table(urbn_type) %>%
                                                              geo %in% unique(urbn_type$geo)) %>% 
                                                dplyr::select(geo, age, sex, popM = pop)), 
                    by = c("geo","sex")) %>% 
-  dplyr::full_join(data.table::as.data.table(harm_data_nuts3_sf) %>%
+  dplyr::full_join(data.table::as.data.table(harm_data_gini_nuts3_sf) %>%
     dplyr::select(-geometry), by = "geo") %>%
+  dplyr::full_join(data.table::as.data.table(nuts3_with_inc_sf) %>%
+    dplyr::select(Inc_nuts3 = inc_pc_nuts3, geo), by = "geo")
+harm_socioeconomic_nuts_sf2 <- harm_socioeconomic_nuts_sf %>% 
   # merge with NUTS3 geometries
   dplyr::left_join(
     nuts3_plot_data %>%
       dplyr::select(geo, geometry),
     by = "geo"
   )
-harm_socioeconomic_nuts_sf <- sf::st_sf(harm_socioeconomic_nuts_sf, geometry = harm_socioeconomic_nuts_sf$geometry)
+harm_socioeconomic_nuts_sf2 <- sf::st_sf(harm_socioeconomic_nuts_sf2, geometry = harm_socioeconomic_nuts_sf2$geometry)
 
-sf::st_write(harm_socioeconomic_nuts_sf, "data/tmp/harm_socioeconomic_nuts_sf_newdeaths_NECP.gpkg", append = FALSE)
+sf::st_write(harm_socioeconomic_nuts_sf2, "data/tmp/harm_socioeconomic_nuts_sf_newdeaths_NECP2.gpkg", append = FALSE)
 
 
 
 ## AP vs socioeconomic DATA ====================================================
 # merge AP & socioeconomic data
 ap_socioecon_sf <- sf::st_intersection(
-  harm_socioeconomic_nuts_sf %>%
+  harm_socioeconomic_nuts_sf2 %>%
     dplyr::filter(sex == 'Both') %>% 
-    dplyr::select(geo, urbn_type, CDD, per_elderly, income = Disp_Inc_P_nuts3,
-                  gini = Gini_nuts3, gdp, geometry) %>%
+    dplyr::select(geo, urbn_type, per_elderly, income = Inc_nuts3,
+                  gini = Gini_nuts3, geometry) %>%
     unique(),
   ap_nuts3_sf %>%
     dplyr::select(ap, ctry = CNTR_CODE, geometry) %>% 
     unique()
 )
-save(ap_socioecon_sf, file = 'ap_socioecon_sf.RData')
+save(ap_socioecon_sf, file = 'ap_socioecon_sf2.RData')
 
 ## DEATHS vs socioeconomic DATA ====================================================
 # merge DEATHS & socioeconomic data
-a = harm_socioeconomic_nuts_sf %>%
+a = harm_socioeconomic_nuts_sf2 %>%
   dplyr::filter(sex == 'Both') %>% 
-  dplyr::select(geo, urbn_type, CDD, per_elderly, income = Disp_Inc_P_nuts3,
-                gini = Gini_nuts3, gdp, geometry) %>%
+  dplyr::select(geo, urbn_type, per_elderly, income = Inc_nuts3,
+                gini = Gini_nuts3, geometry) %>%
   unique()
 
 b = deaths_nuts3_sf %>%
@@ -479,4 +517,4 @@ b = deaths_nuts3_sf %>%
 deaths_socioecon_sf <- sf::st_intersection(
   a,b
 )
-save(deaths_socioecon_sf, file = 'deaths_socioecon_sf_newdeaths.RData')
+save(deaths_socioecon_sf, file = 'deaths_socioecon_sf_newdeaths2.RData')
